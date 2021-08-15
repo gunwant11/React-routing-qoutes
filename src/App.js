a20afcf64b827f9b53b603c04a2b409599499c72
gunwant11 <gunwant7272@gmail.com>
2021-08-15 19:40:15 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import Layout from "./components/layout/Layout";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+import Layout from './components/layout/Layout';
+
+const NewQuote = React.lazy(()=> import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(()=> import('./pages/QuoteDetail'));
+const NotFound = React.lazy(()=> import('./pages/NotFound'));
+const AllQuotes = React.lazy(()=> import('./pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
-      
+      <Suspense fallback={<div className='centered'> <LoadingSpinner /> </div>}  >
       <Switch>
         <Route path="/" exact>
           <Redirect to="quotes"/>
@@ -26,6 +29,7 @@ function App() {
           d<NotFound />
         </Route>
       </Switch>
+      </Suspense>
     </Layout>
   );
 }
